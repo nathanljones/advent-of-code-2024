0a0313c405458c009c70176dcc0ac958ac0aa9c0
nathanljones <nathan.l.jones+github@protonmail.com>
2024-12-14 12:11:54 +0000

    Day 1 fix clippy lints

--- day_01/src/bin/part_1.rs
+++ day_01/src/bin/part_1.rs
@@ -5,20 +5,18 @@ fn main() {
 }
 
 fn find_total_distance(input: &str) -> u32 {
-    let mut lhs: Vec<u32> = Vec::new();
-    let mut rhs: Vec<u32> = Vec::new();
     let mut total: u32 = 0;
 
     let (mut lhs, mut rhs) = parse_input(input);
 
-    lhs.sort();
-    rhs.sort();
+    lhs.sort_unstable();
+    rhs.sort_unstable();
 
     for (counter, lhs_number) in lhs.iter().enumerate() {
         let rhs_number = rhs[counter];
         let difference: i32 = *lhs_number as i32 - rhs_number as i32;
         if difference < 0 {
-            total += (difference * -1) as u32;
+            total += -difference as u32;
         } else {
             total += difference as u32;
         }
@@ -40,11 +38,12 @@ fn parse_input(input: &str) -> (Vec<u32>, Vec<u32>) {
         }
     }
 
-    return (lhs, rhs);
+    (lhs, rhs)
 }
+#[cfg(test)]
 mod tests {
     use super::*;
-    const test_input: &str = "3   4
+    const TEST_INPUT: &str = "3   4
 4   3
 2   5
 1   3
@@ -53,16 +52,16 @@ mod tests {
 
     #[test]
     fn find_correct_answer() {
-        assert_eq!(11, find_total_distance(&test_input));
+        assert_eq!(11, find_total_distance(TEST_INPUT));
     }
     #[test]
     fn test_parsing_line() {
         let test_lhs: Vec<u32> = vec![3, 4, 2, 1, 3, 3];
-        let (lhs, rhs) = parse_input(test_input);
+        let (lhs, _rhs) = parse_input(TEST_INPUT);
         assert_eq!(test_lhs, lhs);
 
         let test_rhs: Vec<u32> = vec![4, 3, 5, 3, 9, 3];
-        let (lhs, rhs) = parse_input(test_input);
+        let (_lhs, rhs) = parse_input(TEST_INPUT);
         assert_eq!(test_rhs, rhs);
     }
 }
