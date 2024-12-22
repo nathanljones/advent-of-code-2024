d50702bbb42c281c94d59428da7abd07b868f679
nathanljones <nathan.l.jones+github@protonmail.com>
2024-12-22 18:44:31 +0000

    Day 6 part 2 - put back original algorithm & resolve clippy lints

--- day_06/src/bin/part_2.rs
+++ day_06/src/bin/part_2.rs
@@ -43,45 +43,29 @@ fn count_distinct_positions(input: &str) -> u32 {
         if will_hit_object(current_position, &current_direction, &map) {
             current_direction = current_direction.new_direction();
         }
-        current_position = move_guard(current_position, &current_direction);
+        current_position = move_guard(current_position, current_direction);
     }
 
     map.iter().filter(|&(_, v)| *v == 'X').count() as u32 + 1
 }
-fn count_no_obstacles(input: &str) -> u64 {
+fn count_no_obstacles(input: &str) -> u32 {
     let map = parse(input);
     let grid_size = get_grid_size(input);
     let start_position = get_current_position(&map);
     let mut current_position = start_position;
     let mut current_direction = Direction::Up;
     let mut guard_route: HashMap<UVec2, char> = HashMap::new();
-    let mut total_loops: u64 = 0;
-
-    for y in (0..grid_size.y) {
-        for x in (0..grid_size.x) {
-            if start_position.x == x && start_position.y == y {
-                continue;
-            }
-            let mut new_map = map.clone();
-            new_map.insert(UVec2::new(x, y), '#');
-            if will_form_a_loop(&new_map, grid_size) {
-                //println!("X = {}, Y = {}", cell.0.x, cell.0.y);
-                total_loops += 1;
-                continue;
-            }
-        }
-    }
-
-    /*// first find the route guard will go on
+    let mut total_loops: u32 = 0;
+    // first find the route guard will go on
     while !will_leave_grid(current_position, &current_direction, grid_size) {
         guard_route.insert(current_position, 'X');
         if will_hit_object(current_position, &current_direction, &map) {
             current_direction = current_direction.new_direction();
         }
-        current_position = move_guard(current_position, &current_direction);
+        current_position = move_guard(current_position, current_direction);
     }
     guard_route.insert(current_position, 'X');
-    for cell in guard_route.iter() {
+    for cell in &guard_route {
         if *cell.0 == start_position {
             continue;
         }
@@ -89,26 +73,20 @@ fn count_no_obstacles(input: &str) -> u64 {
         let mut new_map = map.clone();
         new_map.insert(*cell.0, '#');
         if will_form_a_loop(&new_map, grid_size) {
-            //println!("X = {}, Y = {}", cell.0.x, cell.0.y);
             total_loops += 1;
         }
-    }*/
+    }
     total_loops
 }
 
 fn will_form_a_loop(map: &HashMap<UVec2, char>, grid_size: UVec2) -> bool {
-    let start_position = get_current_position(&map);
+    let start_position = get_current_position(map);
     let mut current_position = start_position;
     let mut current_direction = Direction::Up;
     let mut guard_route: HashMap<UVec2, (char, Vec<Direction>)> = HashMap::new();
-    let mut count: u32 = 0;
 
     while !will_leave_grid(current_position, &current_direction, grid_size) {
-        if count > 25000 {
-            return true;
-        }
-        count += 1;
-        /*if let Some(current_cell) = guard_route.get(&current_position) {
+        if let Some(current_cell) = guard_route.get(&current_position) {
             if (current_cell.0 == 'X') && current_cell.1.contains(&current_direction) {
                 return true;
             } else if current_cell.0 == 'X' {
@@ -117,22 +95,20 @@ fn will_form_a_loop(map: &HashMap<UVec2, char>, grid_size: UVec2) -> bool {
                 guard_route.insert(current_position, ('X', new_directions));
             }
         } else {
-            let mut new_directions = Vec::new();
-            new_directions.push(current_direction);
+            let new_directions: Vec<Direction> = vec![current_direction];
             guard_route.insert(current_position, ('X', new_directions));
-        } */
-        if will_hit_object(current_position, &current_direction, &map) {
+        }
+        if will_hit_object(current_position, &current_direction, map) {
             current_direction = current_direction.new_direction();
         }
-        if will_hit_object(current_position, &current_direction, &map) {
+        if will_hit_object(current_position, &current_direction, map) {
             current_direction = current_direction.new_direction();
         }
-        if will_hit_object(current_position, &current_direction, &map) {
+        if will_hit_object(current_position, &current_direction, map) {
             current_direction = current_direction.new_direction();
         }
-        current_position = move_guard(current_position, &current_direction);
+        current_position = move_guard(current_position, current_direction);
     }
-    //print!("{}", count);
     false
 }
 fn will_leave_grid(current_pos: UVec2, direction: &Direction, grid_size: UVec2) -> bool {
@@ -162,7 +138,7 @@ fn will_leave_grid(current_pos: UVec2, direction: &Direction, grid_size: UVec2) 
     result
 }
 
-fn move_guard(current_pos: UVec2, direction: &Direction) -> UVec2 {
+fn move_guard(current_pos: UVec2, direction: Direction) -> UVec2 {
     let new_pos = current_pos.as_ivec2() + direction.offset();
     new_pos.as_uvec2()
 }
@@ -260,10 +236,10 @@ mod tests {
     #[test]
     fn test_move_guard() {
         let position = UVec2::new(9, 2);
-        assert_eq!(move_guard(position, &Direction::Up), UVec2::new(9, 1));
-        assert_eq!(move_guard(position, &Direction::Down), UVec2::new(9, 3));
-        assert_eq!(move_guard(position, &Direction::Left), UVec2::new(8, 2));
-        assert_eq!(move_guard(position, &Direction::Right), UVec2::new(10, 2));
+        assert_eq!(move_guard(position, Direction::Up), UVec2::new(9, 1));
+        assert_eq!(move_guard(position, Direction::Down), UVec2::new(9, 3));
+        assert_eq!(move_guard(position, Direction::Left), UVec2::new(8, 2));
+        assert_eq!(move_guard(position, Direction::Right), UVec2::new(10, 2));
     }
     #[test]
     fn test_count_distinct_positions() {
